a7c123dc9963ad9f5939da0688c17bfb62c9ccc5
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-10-07 20:45:36 -0300

    Seguridad: Eliminar clave de API hardcodeada de firebase-messaging-sw.js

--- app_agendamiento/web/firebase-messaging-sw.js
+++ app_agendamiento/web/firebase-messaging-sw.js
@@ -1,16 +1,8 @@
 // Scripts for firebase and firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+importScripts('/__/firebase/init.js');
 
-// Initialize the Firebase app in the service worker with your project's configuration
-firebase.initializeApp({
-  apiKey: "AIzaSyCWn41yfy4LNG1hoBHVJlPWpA29F6b_7ww",
-  appId: "1:994018420997:web:3cfb8ba107a564fa0baf6c",
-  messagingSenderId: "994018420997",
-  projectId: "appagendamiento-ddbd0",
-  authDomain: "appagendamiento-ddbd0.firebaseapp.com",
-  storageBucket: "appagendamiento-ddbd0.firebasestorage.app",
-});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
